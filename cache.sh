9f2b99e1186201334ee5ec1462b43915416fc967
Helmut Hummel <typo3@helhum.io>
2021-07-22 15:19:46 +0200

    Change cache entry every time

--- cache.sh
+++ cache.sh
@@ -5,19 +5,20 @@ echo 'before write'
 ls -la $1
 ls -la $1/foo/bar
 
-echo $2 > $1/current
-
 if [ -f "$1/foo/bar/baz" ]
 then
   echo
   echo 'Cache HIT';
+  [ -f "$1/foo/bar/current" ] && cat $1/foo/bar/current
   echo
 else
   echo 'Cache MISS';
+  [ -f "$1/foo/bar/current" ] && cat $1/foo/bar/current
   touch $1/foo/bar/baz
   echo 'bla' > $1/foo/bar/blupp
 fi
 
 echo 'after write'
+echo $2 > $1/foo/bar/current
 ls -la $1
 ls -la $1/foo/bar
