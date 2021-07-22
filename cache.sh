6576d2f58a3d351a0c43974280326dee1ad9927a
Helmut Hummel <typo3@helhum.io>
2021-07-22 14:57:33 +0200

    Fix cache folder

--- cache.sh
+++ cache.sh
@@ -3,10 +3,14 @@
 mkdir -p $1/foo/bar
 ls -la $1/foo/bar
 
+echo $2 > $1/current
+
 if [ -f "$1/foo/bar/baz" ]
 then
   echo 'cache hit';
 else
+  echo 'cache miss';
   touch $1/foo/bar/baz
   echo 'bla' > $1/foo/bar/blupp
 fi
+
